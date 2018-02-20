faff72a707245fbf1fd95a0ade01bcb4371d7858
Nathan Pankowsky <nathan.pankowsky@gmail.com>
2018-02-20 22:07:17 +0200

    Added doctests and add/sub

--- tools.py
+++ tools.py
@@ -1,6 +1,33 @@
 # basic tools for doing ml
 
 class TwoDArray(object): # yes, I know everything exists in numpy already.
+    """A basic 2d array
+    
+    >>> arr = TwoDArray(2, 2) 
+    >>> print arr # doctest: +NORMALIZE_WHITESPACE
+    [
+    	[0, 0],
+    	[0, 0]
+    ]
+    >>> arr2 = TwoDArray(2, 2, [[1, 1], [2, 3]])
+    >>> print arr2 # doctest: +NORMALIZE_WHITESPACE
+    [
+    	[1, 1],
+    	[2, 3]
+    ]
+    >>> print arr + arr2 # doctest: +NORMALIZE_WHITESPACE
+    [
+    	[1, 1],
+    	[2, 3]
+    ]
+    >>> print arr - arr2 # doctest: +NORMALIZE_WHITESPACE
+    [
+    	[-1, -1],
+    	[-2, -3]
+    ]
+    
+    
+    """
     def __init__(self, m, n, array=None):
         self.m = m
         self.n = n
@@ -10,15 +37,18 @@ class TwoDArray(object): # yes, I know everything exists in numpy already.
         else:
             self._array = [[0 for i in xrange(m)] for j in xrange(n)]
     
-    def singular_value_decomposition(twoDarray):
+    def singular_value_decomposition(self):
         u, sig, v_star = 0, 0, 0
         pass
+        
+    def __str__(self):
+        return "[\n\t{}\n]".format(',\n\t'.join(str(data_set) for data_set in self._array))
         
     def normalize_1d_data(self, data, range_func=None):
         if range_func:
             r = float(range_func(data))
         else:
-            r = max(data) - min(data)
+            r = float(max(data) - min(data))
         mean = sum(data) / float(len(data))
         
         return [(item - mean) / r for item in data]
@@ -29,12 +59,13 @@ class TwoDArray(object): # yes, I know everything exists in numpy already.
     def __foreach(self, func, copy, other):
         for n in xrange(self.n):
             for m in xrange(self.m):
-                copy._array[n][m].func(other[n][m])
+                copy[n, m] = func(copy[n, m], other[n, m])
      
     def normalize(self):
         ret = self.copy()
         
-        #f
+        for n, data_set in enumerate(self._array):
+            ret._array[n] = self.normalize_1d_data(data_set)
         
         return ret
         
@@ -43,20 +74,47 @@ class TwoDArray(object): # yes, I know everything exists in numpy already.
     
     def __setitem__(self, tup, value):
         self._array[tup[0]][tup[1]] = value
+   
+    @staticmethod
+    def __add(obj1, obj2):
+        return obj1 + obj2
         
-    def __add__(self, other):
+    @staticmethod
+    def __sub(obj1, obj2):
+        return obj1 - obj2
+        
+    @staticmethod
+    def __mult(obj1, obj2):
+        return obj1 * obj2
+    
+    def __identical_check(self, other):
         if other.m != self.m and other.n != self.n:
-            raise Exception("Arrays are not of same dimension")
-        
+            raise Exception("Arrays are not of same dimensions!")
+    
+    def __add__(self, other):
+        self.__identical_check(other)
+
         ret = self.copy()
-        
-        self.__foreach(self, __add__, ret, other)
+
+        self.__foreach(self.__add, ret, other)
                
         return ret
-                
+        
+    def __sub__(self, other):
+        self.__identical_check(other)
+
+        ret = self.copy()
+
+        self.__foreach(self.__sub, ret, other)
+               
+        return ret            
         
     def __mul__(self, other):
         pass
         
     def __rmul__(self, other):
         pass
+        
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
